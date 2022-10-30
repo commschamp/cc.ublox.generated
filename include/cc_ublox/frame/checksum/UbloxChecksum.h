7235b146159fca0b5d306ac0f6670daa7eb62cd3
Alex Robenko <arobenko@gmail.com>
2022-10-30 11:01:05 +1000

    Using v2.0 of cc.ublox.commsdsl to generate current code.

---
+++ include/cc_ublox/frame/checksum/UbloxChecksum.h
@@ -0,0 +1,47 @@
+
+#pragma once
+
+#include <cstdint>
+#include <limits>
+
+#include "comms/cast.h"
+
+namespace cc_ublox
+{
+
+namespace frame
+{
+
+namespace checksum
+{
+
+/// @brief Checksum calculator.
+/// @details Provided to @b comms::protocol::ChecksumLayer
+///     when defining transport framing (@ref cc_ublox::frame::UbloxFrame).
+struct UbloxChecksum
+{
+    template <typename TIter>
+    std::uint16_t operator()(TIter& iter, std::size_t len) const
+    {
+        std::uint8_t ckA = 0;
+        std::uint8_t ckB = 0;
+        for (auto idx = 0U; idx < len; ++idx) {
+            comms::cast_assign(ckA) = ckA + *iter;
+            comms::cast_assign(ckB) = ckB + ckA;
+            ++iter;
+        }
+
+        return
+            static_cast<std::uint16_t>(
+                (static_cast<std::uint16_t>(ckB) << std::numeric_limits<std::uint8_t>::digits) |
+                    ckA);
+    }
+};
+
+} // namespace checksum
+
+}  // namespace frame
+
+}  // namespace cc_ublox
+
+
